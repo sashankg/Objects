5dc8c4e31415799aa33d1d1680d5d008c54f9712
Sashank Gogula <sashankg@gmail.com>
2015-10-17 13:39:34 -0500

    Initial Commit

---
+++ Objects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Objects
+//
+//  Created by Sashank Gogula on 10/17/15.
+//  Copyright © 2015 sashankg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
