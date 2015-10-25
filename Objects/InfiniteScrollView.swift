383fa379cfcb88abfd207cec50540100da562122
Sashank Gogula <sashankg@gmail.com>
2015-10-24 22:47:15 -0500

    Added animation when too few views

--- Objects/InfiniteScrollView.swift
+++ Objects/InfiniteScrollView.swift
@@ -30,10 +30,10 @@ class InfiniteScrollView: UIScrollView, UIScrollViewDelegate {
 		contentSize = CGSizeMake(frame.width + 1, frame.height + 1)
 		delegate = self
 		
-		for i in 0...5
+		for i in 0...2
 		{
 			views.append([UIView]())
-			for j in 0...5
+			for j in 0...2
 			{
 				let x = CGFloat(i) * (size + space) + CGFloat(j%2) * (size + space)/2
 				let y = CGFloat(j) * (size + space)
@@ -55,39 +55,67 @@ class InfiniteScrollView: UIScrollView, UIScrollViewDelegate {
 	}
 	
 	func scrollViewDidScroll(scrollView: UIScrollView) {
-		if scrollView.contentOffset.x < 0 && views[views.count-1][0].frame.origin.x > frame.width
+		
+		func animateViewEntrance(view: UIView)
+		{
+			let f = view.frame
+			view.frame = CGRectMake(f.origin.x + f.width/2, f.origin.y + f.height/2, 0, 0)
+			UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+				view.frame = f
+				}, completion: nil)
+		}
+		
+		if scrollView.contentOffset.x < 0 && views.last![0].frame.origin.x > frame.width
 		{
 			var shift: CGFloat = 0
-			for i in 0...views[views.count-1].count-1
+			for i in 0...views.last!.count-1
 			{
-				views[views.count-1][i].frame.origin.x = views[0][0].frame.origin.x - size - space + (shift%2) * (size + space)/2
+				let offset = (shift%2) * (size + space)/2
+				views[views.count-1][i].frame.origin.x = views[0][0].frame.origin.x - size - space + offset
+				if views[views.count-1][i].frame.origin.x + size + space + offset > 0
+				{
+					animateViewEntrance(views[views.count-1][i])
+				}
 				shift += 1
 			}
 			views.insert(views.removeLast(), atIndex: 0)
 		}
-		else if scrollView.contentOffset.x > 0 && views[0][1].frame.origin.x + size + space < 0
+		else if scrollView.contentOffset.x > 0 && views[0][1].frame.origin.x + size < 0
 		{
 			var shift: CGFloat = 0
 			for i in 0...views[0].count-1
 			{
-				views[0][i].frame.origin.x = views[views.count-1][0].frame.origin.x + space + size + (shift%2) * (size + space)/2
+				views[0][i].frame.origin.x = views.last![0].frame.origin.x + space + size + (shift%2) * (size + space)/2
+				if views[0][i].frame.origin.x < frame.width
+				{
+					animateViewEntrance(views[0][i])
+				}
 				shift += 1
 			}
 			views.insert(views.removeFirst(), atIndex: views.count)
 		}
 		
-		if scrollView.contentOffset.y < 0 && yViews[yViews.count-1][0].frame.origin.y > frame.height
+		if scrollView.contentOffset.y < 0 && yViews.last![0].frame.origin.y > frame.height
 		{
-			for i in 0...yViews[yViews.count-1].count-1
+			for i in 0...yViews.last!.count-1
 			{
-				yViews[yViews.count-1][i].frame.origin.y = yViews[0][0].frame.origin.y - size - space			}
+				yViews.last![i].frame.origin.y = yViews[0][0].frame.origin.y - size - space
+				if yViews.last![i].frame.origin.y - size > 0
+				{
+					animateViewEntrance(yViews[yViews.count-1][i])
+				}
+			}
 			yViews.insert(yViews.removeLast(), atIndex: 0)
 		}
-		else if scrollView.contentOffset.y > 0 && yViews[0][1].frame.origin.y + size + space < 0
+		else if scrollView.contentOffset.y > 0 && yViews[0][1].frame.origin.y + size < 0
 		{
 			for i in 0...yViews[0].count-1
 			{
-				yViews[0][i].frame.origin.y = yViews[yViews.count-1][0].frame.origin.y + space + size
+				yViews[0][i].frame.origin.y = yViews.last![0].frame.origin.y + space + size
+				if yViews[0][i].frame.origin.y < frame.height
+				{
+					animateViewEntrance(yViews[0][i])
+				}
 			}
 			yViews.insert(yViews.removeFirst(), atIndex: yViews.count)
 		}
