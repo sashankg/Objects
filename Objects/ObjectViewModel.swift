340eafe63474f4e86470569fccda9f8d621ec2aa
Sashank Gogula <sashankg@gmail.com>
2015-10-24 22:50:21 -0500

    Added ViewModel

---
+++ Objects/ObjectViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ObjectViewModel.swift
+//  Objects
+//
+//  Created by Sashank Gogula on 10/24/15.
+//  Copyright © 2015 sashankg. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+class ObjectViewModel: NSObject  {
+		
+}
